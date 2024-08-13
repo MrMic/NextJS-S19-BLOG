3ddc54e4e2129b3a4fe1c107197b377d87391aa3
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-08-13 14:31:24 +0200

    feat(UI/UX): Add the "All Posts" Page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,6 +1,46 @@
+import AllPosts from "@/components/posts/all-posts";
+
+
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Next.js is a React framework for production - it makes building fullstack React apps and API servers a breeze, even for large apps.",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started-with-nextjs-2",
+    title: "Getting Started with NextJS 2",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Next.js is a React framework for production - it makes building fullstack React apps and API servers a breeze, even for large apps.",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started-with-nextjs-3",
+    title: "Getting Started with NextJS 3",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Next.js is a React framework for production - it makes building fullstack React apps and API servers a breeze, even for large apps.",
+    date: "2022-02-10",
+  },
+  {
+    slug: "getting-started-with-nextjs-4",
+    title: "Getting Started with NextJS 4",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Next.js is a React framework for production - it makes building fullstack React apps and API servers a breeze, even for large apps.",
+    date: "2022-02-10",
+  },
+];
 
 function AllPostPage() {
-  return <h1>All Posts</h1>;
+  return (
+    <AllPosts posts={DUMMY_POSTS} />
+  )
 }
 
 export default AllPostPage;
