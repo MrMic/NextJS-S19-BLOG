cb52b174413656387296ad096fba113878af1eb6
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-08-16 12:46:34 +0200

    feat: The "Post Detail" Page

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,6 +1,9 @@
+import PostContent from "@/components/posts/post-detail/post-content";
 
 function PostDetailPage() {
-  return <h1>Post Detail</h1>;
+  return (
+    <PostContent />
+  )
 }
 
 export default PostDetailPage;
