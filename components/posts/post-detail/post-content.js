5afc73528772aecfa7abb92bc015a48b6f13e474
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-08-19 12:17:45 +0200

    feat(UI): Render Markdown as JSX

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,3 +1,5 @@
+import ReactMardown from "react-markdown"
+
 import PostHeader from "./post-header"
 
 import classes from "./post-content.module.css"
@@ -17,7 +19,9 @@ function PostContent() {
   return (
     <article className={classes.content}>
       <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      {DUMMY_POST.content}
+      <ReactMardown>
+        {DUMMY_POST.content}
+      </ReactMardown>
     </article>
   )
 }
