7f789afa04001038be3dbab36d0d1fda985e5a6b
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-08-23 13:15:21 +0200

    feat: Render Dynamic Post Pages & Paths

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,26 +1,19 @@
 import ReactMardown from "react-markdown"
 
 import PostHeader from "./post-header"
-
 import classes from "./post-content.module.css"
 
 
-const DUMMY_POST = {
-  slug: "getting-started-with-nextjs",
-  title: "Getting Started with NextJS",
-  image: "getting-started-nextjs.png",
-  date: "2022-02-10",
-  content: "# This is my first post"
-}
+function PostContent(props) {
+  const { post } = props;
 
-function PostContent() {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      <PostHeader title={post.title} image={imagePath} />
       <ReactMardown>
-        {DUMMY_POST.content}
+        {post.content}
       </ReactMardown>
     </article>
   )
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,9 +1,38 @@
 import PostContent from "@/components/posts/post-detail/post-content";
+import { getPostData, getPostFiles } from "@/lib/posts-util";
 
-function PostDetailPage() {
+function PostDetailPage(props) {
   return (
-    <PostContent />
+    <PostContent post={props.post} />
   )
 }
+
+// ______________________________________________________________________
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData
+    },
+    revalidate: 600
+  };
+}
+
+// ______________________________________________________________________
+export function getStaticPaths() {
+  const postFilenames = getPostFiles();
+
+  const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ''));
+
+  return {
+    paths: slugs.map((slug) => ({ params: { slug: slug } })),
+    fallback: false
+  }
+}
+
 
 export default PostDetailPage;
