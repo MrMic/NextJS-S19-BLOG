777618305e94acdf50c912426c1617b1741b75c0
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-08-28 12:40:16 +0200

    feat(UI/UX): Render Code snippets from Markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,4 +1,6 @@
 import ReactMardown from "react-markdown"
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 import PostHeader from "./post-header"
 import classes from "./post-content.module.css"
@@ -43,17 +45,17 @@ function PostContent(props) {
       return <p>{paragraph.children}</p>;
     },
 
-    // code(code) {
-    //   const { className, children } = code;
-    //   const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
-    //   return (
-    //     <SyntaxHighlighter
-    //       style={atomDark}
-    //       language={language}
-    //       children={children}
-    //     />
-    //   );
-    // },
+    code(code) {
+      const { className, children } = code;
+      const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          showLineNumbers={true}
+        >{children}</SyntaxHighlighter>
+      );
+    },
   };
 
   return (
