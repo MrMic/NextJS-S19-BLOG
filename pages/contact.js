f010a13eac8cd39fe1c08a41539c55aaf94f7643
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-09-06 12:27:28 +0200

    feat(UX): Add head data

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,16 @@
-import ContactForm from "@/components/contact/contact-form";
+import { Fragment } from "react";
 
+import ContactForm from "@/components/contact/contact-form";
+import Head from "next/head";
 function Contact() {
   return (
-    <ContactForm />
+    <Fragment>
+      <Head>
+        <title>Contact Me</title>
+        <meta name="description" content="Send me your messages" />
+      </Head>
+      <ContactForm />
+    </Fragment>
   );
 }
 
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,9 +1,17 @@
 import PostContent from "@/components/posts/post-detail/post-content";
 import { getPostData, getPostFiles } from "@/lib/posts-util";
+import Head from "next/head";
+import { Fragment } from "react";
 
 function PostDetailPage(props) {
   return (
-    <PostContent post={props.post} />
+    <Fragment>
+      <Head>
+        <title>{props.post.title}</title>
+        <meta name="description" content={props.post.excerpt} />
+      </Head>
+      <PostContent post={props.post} />
+    </Fragment>
   )
 }
 
