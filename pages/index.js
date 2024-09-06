e0c90fc90401390399a5a34a20b7ebcbec4f40d7
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-09-06 12:27:28 +0200

    feat(UX): Add head data

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,16 @@ import { Fragment } from "react";
 import Hero from "@/components/home-page/hero";
 import FeaturePosts from "@/components/home-page/featured-posts";
 import { getFeaturedPosts } from "@/lib/posts-util";
+import Head from "next/head";
 
 
 function HomePage(props) {
   return (
     <Fragment>
+      <Head>
+        <title>Mic&apos;s Blog</title>
+        <meta name="description" content="I post about programming and web development" />
+      </Head>
       <Hero />
       <FeaturePosts posts={props.posts} />
     </Fragment>
