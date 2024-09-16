142d1adc9f0801ecb999120cc7793c4da2441f97
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-09-16 12:13:27 +0200

    feat: prepared for deployment

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -6,19 +6,19 @@ dotenv.config();
 import { MongoClient } from "mongodb";
 
 // ! WARN: On the Server Side only!
-async function handler(req, res) {
-  if (req.method === 'POST') {
+async function handler( req, res ) {
+  if ( req.method === 'POST' ) {
     const { email, name, message } = req.body;
 
     if (
       !email ||
-      !email.includes('@') ||
+      !email.includes( '@' ) ||
       !name ||
       name.trim() === '' ||
       !message ||
       message.trim() === ''
     ) {
-      res.status(422).json({ message: 'Invalid input.' });
+      res.status( 422 ).json( { message: 'Invalid input.' } );
       return;
     }
 
@@ -33,32 +33,32 @@ async function handler(req, res) {
     // ______________________________________________________________________
     let client;
     try {
-      client = await MongoClient.connect(process.env.MONGODB_URI);
-    } catch (error) {
-      console.log("🪚 error:", error);
-      res.status(500).json({ message: 'Could not connect to database' });
+      client = await MongoClient.connect( process.env.MONGODB_URI );
+    } catch ( error ) {
+      console.log( "🪚 error:", error );
+      res.status( 500 ).json( { message: 'Could not connect to database' } );
       return;
     }
 
     // * INFO: 'nextjs-blog' is the name of the database
-    const db = process.env.DEV === 'dev' ? client.db('nextjs-blog-dev') : client.db('nextjs-blog');
+    const db = process.env.DEV === 'dev' ? client.db( 'nextjs-blog-dev' ) : client.db( 'nextjs-blog' );
     // const db = client.db('nextjs-blog');
 
     try {
-      const result = await db.collection('messages').insertOne(newMessage);
+      const result = await db.collection( 'messages' ).insertOne( newMessage );
       newMessage._id = result.insertedId;
 
-      console.log("🪚 result:", result);
-    } catch (error) {
-      console.log("🪚 error:", error);
+      console.log( "🪚 result:", result );
+    } catch ( error ) {
+      console.log( "🪚 error:", error );
       client.close();
-      res.status(500).json({ message: 'Storing message failed!' });
+      res.status( 500 ).json( { message: 'Storing message failed!' } );
       return;
     }
 
     client.close();
 
-    res.status(201).json({ message: 'Successfully stored message! 💾', message: newMessage });
+    res.status( 201 ).json( { message: 'Successfully stored message! 💾', message: newMessage } );
 
   }
 }
